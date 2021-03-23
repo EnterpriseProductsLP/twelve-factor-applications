667d670a4c2d3777e97957c1e2c2d8f518bab3b6
Eric Burcham <eburcham@eprod.com>
2021-03-23 16:04:54 -0500

    Add config controller and index view.

---
+++ src/dotnet/ClientUI/Controllers/ConfigController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace ClientUI.Controllers
+{
+    public class ConfigController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult FromAppSettings()
+        {
+            var 
+        }
+    }
+}
