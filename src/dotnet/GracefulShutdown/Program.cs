d86277d69a6f0b2d5b0efc155e4ba5020c160bd7
Eric Burcham <jameseburcham@gmail.com>
2021-03-26 10:36:45 -0500

    Finished graceful shutdown example

---
+++ src/dotnet/GracefulShutdown/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GracefulShutdown
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) => { services.AddHostedService<TimeWriterService>(); });
+        }
+    }
+}
--- src/dotnet/GracefulShutdown/TimeWriterService.cs
+++ src/dotnet/GracefulShutdown/TimeWriterService.cs
@@ -1,7 +1,48 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
 namespace GracefulShutdown
 {
-    public class TimeWriterService
+    public class TimeWriterService : IHostedService
     {
-        
+        private readonly IHostApplicationLifetime _appLifetime;
+        private readonly TimeWriter _timeWriter;
+
+        public TimeWriterService(
+            IHostApplicationLifetime appLifetime)
+        {
+            _appLifetime = appLifetime;
+            _timeWriter = new TimeWriter();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _appLifetime.ApplicationStarted.Register(OnStarted);
+            _appLifetime.ApplicationStopping.Register(OnStopping);
+            _appLifetime.ApplicationStopped.Register(OnStopped);
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private void OnStarted()
+        {
+            _timeWriter.Start();
+        }
+
+        private void OnStopping()
+        {
+            _timeWriter.Stop();
+        }
+
+        private void OnStopped()
+        {
+            _timeWriter.Dispose();
+        }
     }
 }
